6e03ef86068323fe57373ca22c913c4d464fc86f
warbee0712 <emberspirit517@gmail.com>
2025-03-09 00:52:57 -0600

    fix: cmd showing is figured out.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,7 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 use winapi::um::winuser::{
     SetWindowsHookExA,
